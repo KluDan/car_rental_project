88bf11a379e599e4c8f422727fa5fe583f02e817
Danylo Kliushnychenko <danylktv2@gmail.com>
2023-12-11 21:44:12 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/car_rental_project">
+        <BrowserRouter basename="/car_rental_project/">
           <App />
           <GlobalStyles />
         </BrowserRouter>
